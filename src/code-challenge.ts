9260c6167503c2da6a9295e9b0e8aa49d1ffcc02
Tiago <sathoskath@gmail.com>
2025-01-04 23:04:10 -0300

    add code

--- src/code-challenge.ts
+++ src/code-challenge.ts
@@ -24,3 +24,32 @@ const expect = (val1: unknown) => {
     }
   }
 }
+
+function fizzBuzz(n: number) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+      let string = '';
+  
+      if (i % 3 === 0) string += 'Fizz'
+      if (i % 5 === 0) string += 'Buzz'
+  
+      console.log(string || i)
+  }
+}
+
+function getMaxHiddenChips(imageDim: number[], k: number) {
+  // Write your code here
+  let hiddenChipsCount = 0;
+  let chipsCanBeHiddenArr = [...imageDim]
+
+  imageDim.forEach((chipDim) => {
+      const indexOfChipToHide = chipsCanBeHiddenArr.findIndex((chip) => chipDim * k <= chip);
+
+      if (indexOfChipToHide !== -1) {
+          hiddenChipsCount++;
+          chipsCanBeHiddenArr.splice(indexOfChipToHide, 1);
+      }
+  });
+
+  return hiddenChipsCount
+}
